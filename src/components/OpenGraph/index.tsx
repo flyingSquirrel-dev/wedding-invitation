b526b0b9c75c8c9148907cd906a5efb0f2026a31
flyingSquirrel-dev <greatwisd@gmail.com>
2022-08-07 22:30:03 +0900

    feat: og image 넣기

---
+++ src/components/OpenGraph/index.tsx
@@ -0,0 +1,37 @@
+import { ENV } from "constant/env";
+
+const { BRIDE, GROOM } = ENV;
+
+export function OpenGraph() {
+  return (
+    <>
+      <meta
+        property="og:title"
+        content={`${GROOM} 🤍 ${BRIDE} 결혼식에 초대해요.`}
+      />
+      <meta property="og:type" content="website" />
+      {/* TODO: og:url 추가하기 */}
+      <meta property="og:url" content="" />
+      <meta
+        property="og:image"
+        content="https://wisdom-kangto.s3.ap-northeast-2.amazonaws.com/OG_800_400.jpg"
+      />
+      <meta property="og:image:width" content="800" />
+      <meta property="og:image:height" content="400" />
+      <meta
+        property="og:image"
+        content="https://wisdom-kangto.s3.ap-northeast-2.amazonaws.com/OG_1200_630.jpg"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta
+        property="og:image"
+        content="https://wisdom-kangto.s3.ap-northeast-2.amazonaws.com/OG_1200_630.jpg"
+      />
+      <meta
+        property="og:image:alt"
+        content={`${GROOM} 🤍 ${BRIDE} 결혼식에 초대해요.`}
+      />
+    </>
+  );
+}
