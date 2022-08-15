7c2bb447e46b4a75a235009e9f244bf5ab79c11a
kanghyunlee <rkdgusdl92@gmail.com>
2022-08-15 17:25:15 +0900

    feat: add kakao navi, tmap navi - 카카오 내비 길찾기 - 티맵 길찾기

---
+++ src/Kakao.d.ts
@@ -0,0 +1,18 @@
+declare namespace Kakao {
+  export function init(key: string): void;
+  export function isInitialized(): boolean;
+  export function cleanup(): void;
+
+  export interface NaviOptions {
+    name: string;
+    // LNG
+    x: number;
+    // LAT
+    y: number;
+    coordType?: "wgs84" | "katec";
+  }
+
+  export class Navi {
+    static start(options: NaviOptions): void;
+  }
+}
--- src/pages/WeddingInvitation/components/Location.tsx
+++ src/pages/WeddingInvitation/components/Location.tsx
@@ -2,9 +2,10 @@ import Text from "components/Text";
 import { Spacing } from "components/Spacing";
 import { Flex } from "components/Flex";
 import { LocationMap } from "components/LocationMap";
+import { Navi } from "components/Navi";
 import { ENV } from "constant/env";
 
-const { TIME, PLACE, PLACE_LAT, PLACE_LNG } = ENV;
+const { TIME, PLACE_ADDRESS, PLACE_NAME, PLACE_LAT, PLACE_LNG } = ENV;
 
 export function Location() {
   return (
@@ -13,7 +14,7 @@ export function Location() {
         <Text text="오시는 길" size={32} />
         <Spacing size={25} />
         <Text text={`일시: ${TIME}`} />
-        <Text text={`장소: ${PLACE}`} />
+        <Text text={`장소: ${PLACE_ADDRESS} ${PLACE_NAME}`} />
         <Spacing size={25} />
         <LocationMap
           center={{
@@ -21,6 +22,8 @@ export function Location() {
             lng: Number(PLACE_LNG),
           }}
         />
+        <Spacing size={25} />
+        <Navi />
       </Flex>
     </Flex>
   );
